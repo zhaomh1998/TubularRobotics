0694fa3888ff29b846c4a815850a122ff035161f
Scott Zhao <zhaomh1998@outlook.com>
2018-05-27 21:34:14 +0000

    add encoder testing program

---
+++ rotary_encoder.py
@@ -0,0 +1,28 @@
+from RPi import GPIO
+from time import sleep
+
+clk = 19
+dt = 21
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(clk, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(dt, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+counter = 0
+clkLastState = GPIO.input(clk)
+
+try:
+
+        while True:
+                clkState = GPIO.input(clk)
+                dtState = GPIO.input(dt)
+                if clkState != clkLastState:
+                        if dtState != clkState:
+                                counter += 1
+                        else:
+                                counter -= 1
+                        print counter
+                clkLastState = clkState
+                sleep(0.0001)
+finally:
+        GPIO.cleanup()
